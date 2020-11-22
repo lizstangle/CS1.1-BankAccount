86b2ab8678294a729b18da47fd3ac06857e70178
lizstangle <lizstangle@me.com>
2020-11-22 14:08:21 -0800

    methods

--- BankAccount.py
+++ BankAccount.py
@@ -1,10 +1,21 @@
-class BankAccount:
-    def __init__(self, full_name, account_number):
+class BankAccount():
+    def __init__(self, full_name, account_number, routing_number, balance):
         self.full_name = full_name
         self.account_number = account_number
-        self.routing_number = 123456789
-        self.balance = 0
+        self.routing_number = routing_number
+        self.balance = balance
+    
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Amount Deposited: ${amount}")
+        print(f"Your updated balance amount is: {self.balance}")
 
-account = BankAccount("Laura Apple", )
+    def withdraw(self, amount):
+        if self.balance >= amount:
 
-random_account = list(range(10000000,100000000))
+            self.balance -= amount
+            print(f"Amount Withdrawn: ${amount}")
+
+        else:
+            self.balance -= 10
+            return f"Insufficient funds. You have incurred an overdraft fee of $10. Your balance is {self.balance}."
